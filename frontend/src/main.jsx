8e192547d39804ebaff8329297288027fb9303dc
Fizza Alwani <fizzaalwani@gmail.com>
2026-02-06 14:54:23 +0500

    UI changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import "jodit/es5/jodit.min.css";
 import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext.jsx';
+// import { NotesProvider } from './context/NotesContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-       <App />
+    <AuthProvider>
+      {/* <NotesProvider> */}
+           <App />
+      {/* </NotesProvider> */}
+    </AuthProvider>
   </StrictMode>,
 )
